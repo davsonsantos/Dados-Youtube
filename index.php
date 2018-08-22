d9294088ee54fa2656ca16e1730b0cfda4e97b33
Davson Santos <davsonsantos@gmail.com>
2018-08-22 16:06:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $Ytb = new Youtube;
 var_dump($Ytb->getDataVideo('https://www.youtube.com/watch?v=epYKVcHrVr0'));
 
  //Get videos from channel by YouTube Data API
-$API_key = 'AIzaSyAuAfj5iLOO6Okq595tSlw4DG9ijLJUns4';
-$channelID = 'UC15GqFDwpwLmmzkbfXQmi3Q';
+$API_key = '[API-DO-YOUTUBE]';
+$channelID = '[ID-DO-CANAL-DO-YOUTUBE]';
 $maxResults = 10;
 
 $Ytb = new Youtube($API_key, $channelID, $maxResults);
